8f039ff44bea4f20121b99c091691e6558718bb2
Arvind Ashok <117076503+arvindashok@users.noreply.github.com>
2022-12-20 00:25:40 +0530

    Update suppliers_week7.sql

--- suppliers_week7.sql
+++ suppliers_week7.sql
@@ -47,3 +47,36 @@ insert into catalog values
 (10002, 20002, 20),
 (10003, 20003, 30),
 (10004, 20003, 40);
+
+/*3. Find the pnames of parts for which there is some supplier.*/
+select distinct pname from parts p, catalog c where p.pid=c.pid; 
+
+/*4. Find the snames of suppliers who supply every part.*/
+
+select sname from suppliers where sid in
+(select sid from catalog c where c.pid = all(select pid from parts));
+
+select sname from suppliers s where sid in
+(select sid from catalog c where 
+not exists(select pid from parts p where not exists
+(select sid from catalog c1 where c1.sid=c.sid and c1.pid=p.pid)));
+
+/*5. Find the snames of suppliers who supply every red part.*/
+
+select sname from suppliers where sid in
+(select sid from catalog where pid in(select pid from parts where color='Red'));
+
+/*6. Find the pnames of parts supplied by Acme Widget Suppliers and by no
+one else.*/
+
+select pname from parts where pid in
+(select pid from catalog where sid in
+(select sid from suppliers where sname like 'Acme%'));
+
+/*7. Find the sids of suppliers who charge more for some part than the average
+cost of that part (averaged over all the suppliers who supply that part).*/
+
+
+
+/*8. For each part, find the sname of the supplier who charges the most for
+that part.*/
